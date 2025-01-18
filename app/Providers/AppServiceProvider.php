63acfc9df01c70c4402db378d036f2e74b91a3e3
Erik Ieger Dobrychtop <erikdobrychtop@gmail.com>
2025-01-18 03:30:57 -0300

    correção de pastas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,34 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register()
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        // Caminho base dos serviços
-        $servicesPath = app_path('Services');
-
-        // Buscar todos os arquivos PHP no diretório Services
-        $serviceFiles = glob($servicesPath . '/*.php');
-
-        foreach ($serviceFiles as $file) {
-            // Obter o nome completo da classe com base no caminho do arquivo
-            $className = 'App\\Services\\' . basename($file, '.php');
-
-            // Verificar se a classe existe
-            if (class_exists($className)) {
-                // Registrar automaticamente a classe no container
-                $this->app->singleton($className, function ($app) use ($className) {
-                    return new $className();
-                });
-            }
-        }
+        //
     }
 
-    public function boot()
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
     {
-        // Métodos adicionais
+        //
     }
 }
