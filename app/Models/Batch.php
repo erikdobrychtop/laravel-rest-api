568be76b3ade70ed51c13c0a3e7cc02769aa7b95
Erik Ieger Dobrychtop <erikdobrychtop@gmail.com>
2025-01-20 05:01:45 -0300

    melhorias de coluna nova

--- app/Models/Batch.php
+++ app/Models/Batch.php
@@ -10,9 +10,23 @@ class Batch extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name', 'batch', 'map_register', 'production_date',
-        'abv', 'ibu', 'mash_brix', 'pre_boil_brix', 'post_boil_brix',
+        'name', 
+        'batch', 
+        'map_register', 
+        'production_date',
+        'abv', 
+        'ibu', 
+        'mash_brix', 
+        'pre_boil_brix', 
+        'post_boil_brix', 
+        'fermenter_id'
     ];
+    
+    // Adicionar a relação com o fermentador
+    public function fermenter()
+    {
+        return $this->belongsTo(Fermenter::class);
+    }
 
     public function ingredients()
     {
